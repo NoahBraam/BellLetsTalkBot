bf101988969dfb67a0c25100ef32ad08fc3f9870
Noah Braam <nwbraam@gmail.com>
2019-01-30 16:24:08 -0500

    bot to tweet stuff

---
+++ index.js
@@ -0,0 +1,17 @@
+var Twit = require('twit')
+var c = 0.00;
+var T = new Twit({
+    consumer_key:         'jKmd7tILOaTwLmDQ3OfnRlj22',
+    consumer_secret:      'GS7qq0yxXtB7KwWpSV39n7BnydZxIhzMyxQVJi8knq7vnmx1bv',
+    access_token:         '1090715861617324032-BG4aeRkLBRKaTRJbWayLIKHD3Ix8Dm',
+    access_token_secret:  'cneZjZJxKOpp7ZgGCauvko3Km5hJW1hfesoihjnf7PRFJ',
+})
+
+function tweetFunc() {
+  c+= 0.05;
+  T.post('statuses/update', { status: 'This bot has donated $' + c + 'for #BellLetsTalk'  }, function(err, data, response) {
+    console.log(data);
+  }
+}
+
+setInterval(tweetFunc, 60000);
